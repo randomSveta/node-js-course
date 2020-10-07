866d8a98cc7d92545fe4dcd0513d727eb6b48148
Sveta K <test@test.test>
2020-10-07 17:06:21 +0100

    init: start task 2 and add user endpoints

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,43 @@
+const USERS = [
+  { id: 1, name: 'Amy' },
+  { id: 2, name: 'Jess' },
+  { id: 3, name: 'Daria' }
+];
+const User = require('./user.model');
+
 const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+  return USERS;
 };
 
-module.exports = { getAll };
+const createUser = async user => {
+  const createdUser = new User(user);
+  USERS.push(createdUser);
+  return createdUser;
+};
+
+const getUser = async id => {
+  return USERS.filter(user => user.id === id)[0];
+};
+
+const updateUser = async (id, updatedUser) => {
+  USERS.map(user => {
+    if (user.id === id) {
+      if (updatedUser.name !== user.name) user.name = updatedUser.name;
+      if (updatedUser.login !== user.login) user.login = updatedUser.login;
+      if (updatedUser.password !== user.password) {
+        user.password = updatedUser.password;
+      }
+    }
+    return user;
+  });
+  return USERS.filter(user => user.id === id)[0];
+};
+
+const deleteUser = async id => {
+  USERS.forEach((user, index, array) => {
+    if (user.id === id) array.splice(index, 1);
+  });
+  return 'The user has been deleted';
+};
+
+module.exports = { getAll, getUser, createUser, updateUser, deleteUser };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,27 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
   res.json(users.map(User.toResponse));
+});
+
+router.route('/:id').get(async (req, res) => {
+  const user = await usersService.getUser(req.params.id);
+  res.status(200).send(User.toResponse(user));
+});
+
+router.route('/').post(async (req, res) => {
+  const user = await usersService.createUser(req.body);
+  res.status(200).send(User.toResponse(user));
+});
+
+router.route('/:id').put(async (req, res) => {
+  const user = await usersService.updateUser(req.params.id, req.body);
+  res.status(200).send(User.toResponse(user));
+});
+
+router.route('/:id').delete(async (req, res) => {
+  const message = await usersService.getUser(req.params.id);
+  res.status(204).send(message);
 });
 
 module.exports = router;
